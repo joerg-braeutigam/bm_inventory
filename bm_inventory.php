75c6615d8b05b06869fcedb5ff10023e06775d93
Joerg Braeutigam <braeutigam.joerg@gmail.com>
2023-08-03 09:05:16 +0200

    2023-08-03

--- bm_inventory.php
+++ bm_inventory.php
@@ -9,7 +9,7 @@ class Bm_Inventory extends Module
     {
         $this->name = 'bm_inventory';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.1.0';
         $this->author = 'Braeutiigam Media';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
@@ -49,18 +49,22 @@ class Bm_Inventory extends Module
     {
         $id_lang = (int) Configuration::get('PS_LANG_DEFAULT');
 
-        $sql = "SELECT stock.id_product as pr_id, stock.id_product_attribute, stock.quantity as anzahl, 
-                pl.`name` as product, cl.`name` as kategorie, pr.price as vk, pr.wholesale_price as ek 
-                FROM ps_stock_available stock
-                INNER JOIN ps_product pr ON stock.id_product = pr.id_product
-                INNER JOIN ps_product_lang pl ON pr.id_product = pl.id_product
-                INNER JOIN ps_category_lang cl ON cl.id_category = pr.id_category_default
-                WHERE stock.quantity > 0 AND stock.id_product_attribute = 0 AND pr.active = '1' AND cl.id_lang = '" . $id_lang . "'
-                ORDER BY cl.`name`";
+        $rows = $this->get_products($id_lang);
 
-        $rows = Db::getInstance()->executeS($sql);
+        $new = [];
+        foreach ($rows as $row) {
+            if ($row['type'] == "combinations") {
+                $new = array_merge($new, $this->get_product_combination($row, $id_lang));
+            } else {
+                $new[] = $row;
+            }
+        }
 
-        $output = $rows ? $this->generateTable($rows) : $this->displayInfoMessage($sql);
+
+        // $output .= "<pre>" . print_r($new, 1) . "</pre>";
+        // $output = "<pre>" . print_r($rows, 1) . "</pre>";
+
+        $output = $new ? $this->generateTable($new) : $this->displayInfoMessage("Fehler!");
 
         $baseUrl = Context::getContext()->shop->getBaseURL(true);
         $moduleFolder = $this->_path;
@@ -72,10 +76,115 @@ class Bm_Inventory extends Module
         return $output;
     }
 
+    protected function get_product_combination($row, $id_lang)
+    {
+        $sql_add = "select 
+                stock.id_product, 
+                stock.quantity, 
+                stock.id_product_attribute, 
+                attr.ean13 as ean,
+                group_lang.name as wert,
+                attr_lang.name as inhalt
+            from 
+                ps_stock_available stock, 
+                ps_product_attribute attr, 
+                ps_product_attribute_combination combi,
+                ps_attribute attribute,
+                ps_attribute_lang as attr_lang,
+                ps_attribute_group_lang group_lang
+            where 
+                stock.id_product = '" . $row['id'] . "' and 
+                stock.id_product_attribute != '0' and 
+                stock.id_product_attribute = attr.id_product_attribute and 
+                attr.id_product_attribute = combi.id_product_attribute and 
+                combi.id_attribute = attribute.id_attribute and 
+                attribute.id_attribute_group = group_lang.id_attribute_group and 
+                attr_lang.id_attribute = attribute.id_attribute and 
+                group_lang.id_lang = '" . $id_lang . "' and 
+                attr_lang.id_lang = '" . $id_lang . "'";
+        $add = Db::getInstance()->executeS($sql_add);
+
+        $new = [];
+        $id_product_attribute = 0;
+        for ($i = 0; $i < count($add); $i++) {
+            if ($add[$i]['quantity'] > 0) {
+                if ($id_product_attribute != $add[$i]['id_product_attribute']) { // neue Variante
+                    $id_product_attribute = $add[$i]['id_product_attribute'];
+
+                    $new[$id_product_attribute] = array_merge($add[$i], $row);
+                    $new[$id_product_attribute]['anzahl'] = $add[$i]['quantity'];
+
+                    // variante
+                    $new[$id_product_attribute]['variante'] = $add[$i]['wert'] . ": " . $add[$i]['inhalt'];
+                    unset($new[$id_product_attribute]['wert']);
+                    unset($new[$id_product_attribute]['inhalt']);
+                } else {
+
+                    // variante
+                    $new[$id_product_attribute]['variante'] .= " / " . $add[$i]['wert'] . ": " . $add[$i]['inhalt'];
+                    unset($new[$id_product_attribute]['wert']);
+                    unset($new[$id_product_attribute]['inhalt']);
+                }
+                unset($new[$id_product_attribute]['quantity']);
+            }
+        }
+
+        return $new;
+    }
+
+    protected function get_products($id_lang)
+    {
+        $sql = "SELECT 
+            cl.`name` as kategorie, 
+            stock.id_product as id, 
+            pl.`name` as product, 
+            stock.quantity as anzahl, 
+            round(ph.wholesale_price, 2) as ek, 
+            round(ph.price * 1.19, 2) as vk, 
+            pr.product_type as type
+
+
+            FROM ps_stock_available stock
+                    INNER JOIN ps_product_shop ph ON stock.id_product = ph.id_product
+                    INNER JOIN ps_product as pr ON stock.id_product = pr.id_product
+                    INNER JOIN ps_product_lang pl ON pr.id_product = pl.id_product
+                    INNER JOIN ps_category_lang cl ON cl.id_category = pr.id_category_default
+            WHERE 
+                stock.quantity > 0 AND 
+                stock.id_product_attribute = 0 AND 
+                pr.active = '1' AND 
+                pl.id_lang = '$id_lang' AND
+                cl.id_lang = '$id_lang'
+            ORDER BY cl.`name`";
+
+        $rows = Db::getInstance()->executeS($sql);
+
+        // Brand holen 
+        for ($i = 0; $i < count($rows); $i++) {
+            $sql_brand = "select m.id_manufacturer, m.`name` from ps_manufacturer m, ps_product pr
+                    where pr.id_manufacturer = m.id_manufacturer
+                    and pr.id_product = '" . $rows[$i]['id'] . "'";
+            $query_brand = Db::getInstance()->executeS($sql_brand);
+            $rows[$i]['brand'] = $query_brand[0]['name'];
+        }
+
+        return $rows;
+    }
+
     protected function generateTable($rows)
     {
-        $output .= '<table border="1">';
-        $output .= '<tr><th> &nbsp; Kategorie Name</th><th> &nbsp; Produkt Name</th><th>Produkt Variante</th><th>Anzahl in Stock</th><th>Einkaufspreis netto (Einzel)</th><th>Einkaufspreis netto (Anzahl)</th><th>VK Brutto (Einzel)</th><th>VK Brutto (Gesamt)</th></tr>';
+        $output .= '<table border="1" valign="left" align="top">';
+        $output .= '<tr><th> &nbsp; Kategorie Name &nbsp; </th>
+                        <th> &nbsp; Marke &nbsp; </th>
+                        <th> &nbsp; EAN &nbsp; </th>
+                        <th> &nbsp; Produkt Name &nbsp; </th>
+                        <th> &nbsp; Größe / Farbe &nbsp; </th>
+                        <th> &nbsp; Anzahl im Lager &nbsp; </th>
+                        <th> &nbsp; EK netto (Einzel) &nbsp; </th>
+                        <th> &nbsp; EK netto (Anzahl) &nbsp; </th>
+                        <th> &nbsp; VK Brutto (Einzel) &nbsp; </th>
+                        <th> &nbsp; VK Brutto (Gesamt) &nbsp; </th>
+                    </tr>';
 
         $currentCategory = '';
         $sum_ek = 0;
@@ -87,7 +196,7 @@ class Bm_Inventory extends Module
         foreach ($rows as $row) {
             if (($currentCategory != $row['kategorie']) && $currentCategory != '') {
                 // summary of category 
-                $output .= '<tr><td colspan="2"><strong> &nbsp; '
+                $output .= '<tr><td colspan="4"><strong> &nbsp; '
                     . $currentCategory . ' (am Lager): </strong></td><td></td><td align="right"><strong>'
                     . $cat_anz . ' &nbsp; </strong></td><td></td><td align="right"><strong>'
                     . number_format($cat_ek, 2) . ' EUR &nbsp; </strong></td><td></td><td align="right"><strong>'
@@ -103,33 +212,34 @@ class Bm_Inventory extends Module
 
             $currentCategory = $row['kategorie'];
             $sum_ek += $row['ek'] * $row['anzahl'];
-            $sum_vk += $row['vk'] * $row['anzahl'] * 1.19;
+            $sum_vk += $row['vk'] * $row['anzahl'];
             $sum_anz += $row['anzahl'];
 
             $cat_ek += $row['ek'] * $row['anzahl'];
-            $cat_vk += $row['vk'] * $row['anzahl'] * 1.19;
+            $cat_vk += $row['vk'] * $row['anzahl'];
             $cat_anz += $row['anzahl'];
 
             $context = $this->context;
             $link_tmp = $context->link->getAdminLink('AdminProducts'); //  . '/id_product=' . $product->id; //  . '?#tab-step1';
             $link_array = explode("?", $link_tmp);
-            $link = $link_array[0] . "/" . $row['pr_id'] . "?" . $link_array[1] . "#tab-step1";
+            $link = $link_array[0] . "/" . $row['id'] . "?" . $link_array[1] . "#tab-step1";
 
-            $output .= '<tr><td> &nbsp; '
-                . $row['kategorie'] . '</td><td> &nbsp; '
-                . '<a href="' . $link . '" target="_blank">'
-                . $row['product'] . ' </a></td><td>'
-                . $row['product_variant'] . '</td><td align="right">'
-                . $row['anzahl'] . ' &nbsp; </td><td align="right">'
-                . number_format($row['ek'], 2) . ' EUR &nbsp; </td><td align="right">'
-                . number_format($row['ek'] * $row['anzahl'], 2) . ' EUR &nbsp; </td><td align="right">'
-                . number_format($row['vk'] * 1.19, 2) . ' EUR &nbsp; </td><td align="right">'
-                . number_format($row['vk'] * $row['anzahl'] * 1.19, 2) . ' EUR &nbsp; </td></tr>';
-
+            $output .= '<tr> '
+                . ' <td> &nbsp; ' . $row['kategorie'] . '</td>'
+                . ' <td> &nbsp; ' . $row['brand'] . '<br></td>'
+                . ' <td> &nbsp; ' . $row['ean'] . '<br></td>'
+                . ' <td> &nbsp; <a href="' . $link . '" target="_blank">' . $row['product'] . '  &nbsp; </a></td> '
+                . ' <td> &nbsp; ' . $row['variante'] . ' &nbsp; </td> '
+                . ' <td align="right">' . $row['anzahl'] . ' &nbsp; </td> '
+                . ' <td align="right">' . number_format($row['ek'], 2) . ' EUR &nbsp; </td> '
+                . ' <td align="right">' . number_format($row['ek'] * $row['anzahl'], 2) . ' EUR &nbsp; </td> '
+                . ' <td align="right">' . number_format($row['vk'], 2) . ' EUR &nbsp; </td> '
+                . ' <td align="right">' . number_format($row['vk'] * $row['anzahl'], 2) . ' EUR &nbsp; </td>
+                </tr>';
         }
 
         // letzte Kategorie
-        $output .= '<tr><td colspan="2"><strong> &nbsp; '
+        $output .= '<tr><td colspan="4"><strong> &nbsp; '
             . $currentCategory . ' (am Lager): </strong></td><td></td><td align="right"><strong>'
             . $cat_anz . ' &nbsp; </strong></td><td></td><td align="right"><strong>'
             . number_format($cat_ek, 2) . ' EUR &nbsp; </strong></td><td></td><td align="right"><strong>'
@@ -137,7 +247,7 @@ class Bm_Inventory extends Module
 
         $output .= '<tr><td>&nbsp;</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>';
 
-        $output .= '<tr><td colspan="2"><strong> &nbsp; Gesamt (am Lager): </strong></td><td></td><td align="right"><strong>'
+        $output .= '<tr><td colspan="4"><strong> &nbsp; Gesamt (am Lager): </strong></td><td></td><td align="right"><strong>'
             . $sum_anz . ' &nbsp; </strong></td><td></td><td align="right"><strong>'
             . number_format($sum_ek, 2) . ' EUR &nbsp; </strong></td><td></td><td align="right"><strong>'
             . number_format($sum_vk, 2) . ' EUR &nbsp; </strong></td></tr>';
@@ -146,8 +256,6 @@ class Bm_Inventory extends Module
 
         return $output;
     }
-
-
 
     protected function displayInfoMessage($message)
     {
--- download.php
+++ download.php
@@ -1,41 +1,143 @@
 <?php
 // download.php
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header("Location: /");
+    exit;
+}
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=inventar-shop.csv');
 require('../../config/config.inc.php');
 require('../../init.php');
 
+
+$output = fopen('php://output', 'w');
+
 $id_lang = (int) Configuration::get('PS_LANG_DEFAULT');
 
-$sql = "SELECT stock.id_product as pr_id, 
-        cl.`name` as kategorie, 
-        pl.`name` as product, 
-        pr.reference as artnr,
-        stock.quantity as anzahl,
-        replace(pr.wholesale_price, '.', ',') as ek_e,
-        replace(pr.wholesale_price * stock.quantity, '.', ',') as ek_g, 
-        replace(pr.price, '.', ',') as vk_e,
-        replace(pr.price * stock.quantity, '.', ',') as vk_g
-        FROM ps_stock_available stock
-        INNER JOIN ps_product pr ON stock.id_product = pr.id_product
-        INNER JOIN ps_product_lang pl ON pr.id_product = pl.id_product
-        INNER JOIN ps_category_lang cl ON cl.id_category = pr.id_category_default
-        WHERE stock.quantity > 0 AND stock.id_product_attribute = 0 AND cl.id_lang = '" . $id_lang . "'
-        ORDER BY cl.`name`";
+fputcsv($output, array('Kategorie Name', 'Marke', 'EAN', 'ID', 'Produkt Name', 'Größe / Farbe', 'Anzahl im Lager', 'EK netto (Einzel)', 'EK netto (Anzahl)', 'VK Brutto (Einzel)', 'VK Brutto (Gesamt)'));
+
+$sql = "SELECT 
+            cl.`name` as kategorie, 
+            stock.id_product as id, 
+            pl.`name` as product, 
+            stock.quantity as anzahl, 
+            round(ph.wholesale_price, 2) as ek, 
+            round(ph.price * 1.19, 2) as vk, 
+            pr.product_type as type
+
+
+            FROM ps_stock_available stock
+                    INNER JOIN ps_product_shop ph ON stock.id_product = ph.id_product
+                    INNER JOIN ps_product as pr ON stock.id_product = pr.id_product
+                    INNER JOIN ps_product_lang pl ON pr.id_product = pl.id_product
+                    INNER JOIN ps_category_lang cl ON cl.id_category = pr.id_category_default
+            WHERE 
+                stock.quantity > 0 AND 
+                stock.id_product_attribute = 0 AND 
+                pr.active = '1' AND 
+                pl.id_lang = '$id_lang' AND
+                cl.id_lang = '$id_lang'
+            ORDER BY cl.`name`";
 
 $rows = Db::getInstance()->executeS($sql);
 
-arrayToCsvDownload($rows, "inventory_export.csv", ";");
+foreach ($rows as $row) {
+    // Brand holen 
+    $sql_brand = "select m.id_manufacturer, m.`name` from ps_manufacturer m, ps_product pr
+                    where pr.id_manufacturer = m.id_manufacturer
+                    and pr.id_product = '" . $row['id'] . "'";
+    $query_brand = Db::getInstance()->executeS($sql_brand);
+    $row['brand'] = $query_brand[0]['name'];
 
-function arrayToCsvDownload($array, $filename = "export.csv", $delimiter = ";")
-{
-    header('Content-Type: application/csv');
-    header('Content-Disposition: attachment; filename="' . $filename . '";');
+    if ($row['type'] == "combinations") {
+        $data = array(
+            $row['kategorie'],
+            $row['brand'],
+            $row['ean'],
+            $row['id'],
+            $row['product'],
+            $row['variante'],
+            $row['anzahl'],
+            $row['ek'],
+            $row['ek'] * $row['anzahl'],
+            $row['vk'],
+            $row['vk'] * $row['anzahl']
+        );
 
-    $f = fopen('php://output', 'w');
+        // Varianten holen 
+        $sql_add = "select 
+                stock.id_product, 
+                stock.quantity, 
+                stock.id_product_attribute, 
+                attr.ean13 as ean,
+                group_lang.name as wert,
+                attr_lang.name as inhalt
+            from 
+                ps_stock_available stock, 
+                ps_product_attribute attr, 
+                ps_product_attribute_combination combi,
+                ps_attribute attribute,
+                ps_attribute_lang as attr_lang,
+                ps_attribute_group_lang group_lang
+            where 
+                stock.id_product = '" . $row['id'] . "' and 
+                stock.id_product_attribute != '0' and 
+                stock.id_product_attribute = attr.id_product_attribute and 
+                attr.id_product_attribute = combi.id_product_attribute and 
+                combi.id_attribute = attribute.id_attribute and 
+                attribute.id_attribute_group = group_lang.id_attribute_group and 
+                attr_lang.id_attribute = attribute.id_attribute and 
+                group_lang.id_lang = '" . $id_lang . "' and 
+                attr_lang.id_lang = '" . $id_lang . "'";
+        $add = Db::getInstance()->executeS($sql_add);
 
-    // Write header
-    fputcsv($f, array('Artikel ID', 'Kategorie', 'Produkt', 'ArtNr.', 'Anzahl', 'Einkaufspreis (Einzel)', 'Einkaufspreis (Gesamt)', 'VK Brutto (Einzel)', 'VK Brutto (Gesamt)'), $delimiter);
+        $new = [];
+        $id_product_attribute = 0;
 
-    foreach ($array as $line) {
-        fputcsv($f, $line, $delimiter);
+        $var_product = [];
+        for ($i = 0; $i < count($add); $i++) {
+            if ($add[$i]['quantity'] > 0) {
+                if ($id_product_attribute != $add[$i]['id_product_attribute']) { // neue Variante
+                    $id_product_attribute = $add[$i]['id_product_attribute'];
+
+                    $var_product[$id_product_attribute]['anzahl'] = $add[$i]['quantity'];
+                    $var_product[$id_product_attribute]['ean'] = $add[$i]['ean'];
+                    // variante
+                    $var_product[$id_product_attribute]['variante'] = $add[$i]['wert'] . ": " . $add[$i]['inhalt'];
+                } else {
+
+                    // variante
+                    $var_product[$id_product_attribute]['variante'] .= " / " . $add[$i]['wert'] . ": " . $add[$i]['inhalt'];
+                }
+            }
+        }
+        foreach ($var_product as $product) {
+            $data[2] = $product['ean'];
+            $data[5] = $product['variante'];
+            $data[6] = $product['anzahl'];
+            $data[8] = $row['ek'] * $product['anzahl'];
+            $data[10] = $row['vk'] * $product['anzahl'];
+            fputcsv($output, $data);
+            // print_r($data);
+        }
+    } else {
+        $data = array(
+            $row['kategorie'],
+            $row['brand'],
+            $row['ean'],
+            $row['id'],
+            $row['product'],
+            $row['variante'],
+            $row['anzahl'],
+            $row['ek'],
+            $row['ek'] * $row['anzahl'],
+            $row['vk'],
+            $row['vk'] * $row['anzahl']
+        );
+
+        fputcsv($output, $data);
+        // print_r($data);
     }
 }
+
+fclose($output);
